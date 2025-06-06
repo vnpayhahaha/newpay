827e450464c36b5e2e03e1d1eb3d3b0cf2e21568
daxiong <babyseeme@qq.com>
2025-06-06 10:22:10 +0800

    feat(repository): 创建基础仓库类和特性
    - 新增 AbstractPaginator 类实现 LengthAwarePaginator 接口- 新增 ForwardsCalls 特性用于动态调用
    - 新增 BootTrait 特性用于自动启动特性
    - 新增 RepositoryOrderByTrait 特性处理排序
    - 新增 AttachmentRepository 具体仓库类
    - 新增 IRepository 抽象仓库类

---
+++ app/repository/IRepository.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace app\repository;
+
+use app\lib\abstracts\AbstractPaginator;
+use app\repository\Traits\BootTrait;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Collection;
+
+/**
+ * @template T of Model
+ * @property T $model
+ */
+abstract class IRepository
+{
+    use BootTrait;
+
+    public const PER_PAGE_PARAM_NAME = 'per_page';
+
+    public function handleSearch(Builder $query, array $params): Builder
+    {
+        return $query;
+    }
+
+    public function handleItems(Collection $items): Collection
+    {
+        return $items;
+    }
+
+    public function getQuery(): Builder
+    {
+        return $this->model->newQuery();
+    }
+
+    public function perQuery(Builder $query, array $params): Builder
+    {
+        $this->startBoot($query, $params);
+        return $this->handleSearch($query, $params);
+    }
+
+    public function list(array $params = []): Collection
+    {
+        return $this->perQuery($this->getQuery(), $params)->get();
+    }
+
+    public function count(array $params = []): int
+    {
+        return $this->perQuery($this->getQuery(), $params)->count();
+    }
+
+    public function handlePage(\Illuminate\Pagination\LengthAwarePaginator $paginator): array
+    {
+        if ($paginator instanceof AbstractPaginator) {
+            $items = $paginator->getCollection();
+        } else {
+            $items = Collection::make($paginator->items());
+        }
+        return [
+            'list'  => $items->toArray(),
+            'total' => $paginator->total(),
+        ];
+    }
+
+    public function page(array $params = [], ?int $page = null, ?int $pageSize = null): array
+    {
+        $result = $this->perQuery($this->getQuery(), $params)->paginate(
+            perPage: $pageSize,
+            pageName: static::PER_PAGE_PARAM_NAME,
+            page: $page,
+        );
+        return $this->handlePage($result);
+    }
+
+    /**
+     * @return T
+     */
+    public function create(array $data): mixed
+    {
+        // @phpstan-ignore-next-line
+        return $this->getQuery()->create($data);
+    }
+
+    public function deleteById(mixed $id): int
+    {
+        // @phpstan-ignore-next-line
+        return $this->model::destroy($id);
+    }
+
+    /**
+     * @return null|T
+     */
+    public function findByFilter(array $params): mixed
+    {
+        return $this->perQuery($this->getQuery(), $params)->first();
+    }
+
+    /**
+     * @return T
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+}
