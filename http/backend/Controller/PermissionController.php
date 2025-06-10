75c40130ea5814c193b57a99fbce3c0da5caddaa
daxiong <babyseeme@qq.com>
2025-06-10 20:54:04 +0800

    feat(backend): 添加用户角色更新功能
    - 新增用户角色更新接口和相关逻辑
    - 实现用户信息验证和密码更新功能- 优化错误处理和响应格式

--- http/backend/Controller/PermissionController.php
+++ http/backend/Controller/PermissionController.php
@@ -3,6 +3,8 @@
 namespace http\backend\Controller;
 
 use app\controller\BasicController;
+use app\exception\BusinessException;
+use app\lib\enum\ResultCode;
 use app\middleware\AccessTokenMiddleware;
 use app\model\enums\MenuStatus;
 use app\model\enums\RoleStatus;
@@ -10,8 +12,11 @@ use app\repository\MenuRepository;
 use app\repository\RoleRepository;
 use app\router\Annotations\GetMapping;
 use app\router\Annotations\Middleware;
+use app\router\Annotations\PostMapping;
 use app\router\Annotations\RestController;
+use app\service\UserService;
 use DI\Attribute\Inject;
+use Illuminate\Support\Arr;
 use support\Request;
 use support\Response;
 
@@ -25,6 +30,9 @@ class PermissionController extends BasicController
 
     #[Inject]
     protected RoleRepository $roleRepository;
+
+    #[Inject]
+    protected UserService $userService;
 
     #[GetMapping('/menus')]
     public function menus(Request $request): Response
@@ -48,5 +56,32 @@ class PermissionController extends BasicController
                 ? $this->roleRepository->list(['status' => RoleStatus::Normal])
                 : $request->user->getRoles(['name', 'code', 'remark'])
         );
+    }
+
+    #[PostMapping('/roles')]
+    public function update(Request $request): Response
+    {
+        $validator = validate($request->post(), [
+            'nickname'                  => 'sometimes|string|max:255',
+            'new_password'              => 'sometimes|confirmed|string|min:8',
+            'new_password_confirmation' => 'sometimes|string|min:8',
+            'old_password'              => ['sometimes', 'string'],
+            'avatar'                    => 'sometimes|string|max:255',
+            'signed'                    => 'sometimes|string|max:255',
+            'backend_setting'           => 'sometimes|array',
+        ]);
+        if ($validator->fails()) {
+            return $this->error(ResultCode::UNPROCESSABLE_ENTITY, $validator->errors()->first());
+        }
+        $validatedData = $validator->validate();
+        $user = $request->user;
+        if (Arr::exists($validatedData, 'new_password')) {
+            if (!$user->verifyPassword(Arr::get($validatedData, 'old_password'))) {
+                throw new BusinessException(ResultCode::UNPROCESSABLE_ENTITY, trans('old_password_error', [], 'user'));
+            }
+            $validatedData['password'] = $validatedData['new_password'];
+        }
+        $this->userService->updateById($user->id, $validatedData);
+        return $this->success();
     }
 }
