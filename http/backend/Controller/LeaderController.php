f52cdbcbef343cb4b6886832856f6eb434fed93c
daxiong <babyseeme@qq.com>
2025-06-11 16:10:30 +0800

    feat(backend): 添加领导层管理功能
    - 新增 LeaderRepository、LeaderService 和 LeaderController 类
    - 实现领导层的创建、删除和查询功能
    - 添加用户和部门的关联查询
    - 支持按条件筛选查询结果

---
+++ http/backend/Controller/LeaderController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace http\backend\Controller;
+
+use app\controller\BasicController;
+use app\lib\enum\ResultCode;
+use app\middleware\AccessTokenMiddleware;
+use app\router\Annotations\DeleteMapping;
+use app\router\Annotations\GetMapping;
+use app\router\Annotations\Middleware;
+use app\router\Annotations\PostMapping;
+use app\router\Annotations\RestController;
+use app\service\LeaderService;
+use DI\Attribute\Inject;
+use support\Request;
+use support\Response;
+
+#[RestController("/admin")]
+#[Middleware(AccessTokenMiddleware::class)]
+class LeaderController extends BasicController
+{
+
+    #[Inject]
+    protected LeaderService $service;
+
+    #[GetMapping('/leader/list')]
+    public function pageList(Request $request): Response
+    {
+        return $this->success(data: $this->service->page(
+            $request->all(),
+            $this->getCurrentPage(),
+            $this->getPageSize()
+        ));
+    }
+
+    // create
+    #[PostMapping('/leader')]
+    public function create(Request $request): Response
+    {
+        $validator = validate($request->all(), [
+            'user_id' => 'required|array',
+            'dept_id' => 'required|integer',
+        ]);
+        if ($validator->fails()) {
+            return $this->error(ResultCode::UNPROCESSABLE_ENTITY, $validator->errors()->first());
+        }
+        $validatedData = $validator->validate();
+        $this->service->create(array_merge(
+            $validatedData,
+            [
+                'created_by' => $request->user->id,
+            ]
+        ));
+        return $this->success();
+    }
+
+    #[DeleteMapping('/leader')]
+    public function delete(Request $request): Response
+    {
+        $this->service->deleteByDoubleKey($request->all());
+        return $this->success();
+    }
+}
