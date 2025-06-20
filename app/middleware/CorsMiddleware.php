9f1ed60334f02f40847e4bdda79bd275a9118707
daxiong <babyseeme@qq.com>
2025-06-21 07:27:02 +0800

    refactor(app): 优化跨域中间件处理逻辑
    - 修改了 CorsMiddleware 中对 OPTIONS 请求的处理方式
    - 添加了 Access-Control-Max-Age 头，以缓存预检请求
    - 更新了 middleware 配置，启用了 CorsMiddleware

--- app/middleware/CorsMiddleware.php
+++ app/middleware/CorsMiddleware.php
@@ -11,17 +11,24 @@ class CorsMiddleware implements MiddlewareInterface
     public function process(Request $request, callable $handler): Response
     {
         // 如果是options请求则返回一个空响应，否则继续向洋葱芯穿越，并得到一个响应
-        $response = $request->method() == 'OPTIONS' ? response('') : $handler($request);
+        $response = $handler($request);
 
         // 设置跨域响应头
         $response->withHeaders([
-            'Access-Control-Allow-Credentials' => 'true',// 如果需要携带cookie等凭证信息
+//            'Access-Control-Allow-Credentials' => 'true',// 如果需要携带cookie等凭证信息
             'Access-Control-Allow-Origin'      => '*', // 允许所有域名访问，生产环境应指定具体域名
             'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Token',
             'Access-Control-Expose-Headers'    => 'Authorization, Token',
-            'Access-Control-Max-Age'           => 1728000, // 预检请求缓存时间(秒)
+
         ]);
+        if ($request->method() === 'OPTIONS') {
+            //     'Access-Control-Max-Age'           => 1728000, // 预检请求缓存时间(秒)
+            $response->withHeaders([
+                'Access-Control-Max-Age'           => 1728000,
+            ]);
+            return response('', 204);
+        }
 
         return $response;
     }
