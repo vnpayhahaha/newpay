6414e6756845830f0997d2e544e0fe33473a494b
daxiong <babyseeme@qq.com>
2025-06-27 02:43:54 +0800

    refactor(config): 启用 CORS 配置
    - 移除了路由配置文件中的注释
    - 启用了 OPTIONS 方法的路由处理，用于设置跨域资源共享（CORS）头部
    - 允许所有来源的跨域请求
    - 支持的 HTTP 方法包括 GET, POST, PUT, DELETE, PATCH, OPTIONS
    - 允许的请求头包括 Content-Type, Authorization, X-Requested-With, Token, accept-language
    - 暴露的响应头包括 Authorization, Token, X-Requested-With, accept-language, X-Request-Id
    - 允许携带用户凭证的请求

--- config/route.php
+++ config/route.php
@@ -1,15 +1,15 @@
 <?php
 
-//use Webman\Http\Response;
-//use Webman\Route;
+use Webman\Http\Response;
+use Webman\Route;
 
-//Route::options('[{path:.+}]', function ($request) {
-//    return new Response(204, [
-//        'Access-Control-Allow-Origin'      => '*',
-//        'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-//        'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Token, accept-language',
-//        'Access-Control-Expose-Headers'    => 'Authorization, Token, X-Requested-With, accept-language, X-Request-Id',
-//        'Access-Control-Allow-Credentials' => 'true',
-//    ]);
-//});
+Route::options('[{path:.+}]', function ($request) {
+    return new Response(204, [
+        'Access-Control-Allow-Origin'      => '*',
+        'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Token, accept-language',
+        'Access-Control-Expose-Headers'    => 'Authorization, Token, X-Requested-With, accept-language, X-Request-Id',
+        'Access-Control-Allow-Credentials' => 'true',
+    ]);
+});
 \app\router\AnnotationProvider::start();
