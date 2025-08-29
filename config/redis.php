db20d1c61e0d8da1af79b7201fa0cfb561d6c76b
daxiong <vnpay@pron.me>
2025-08-29 16:42:12 +0800

    feat(config): 新增 Redis队列配置并调整 Redis 缓存配置
    - 新增 queue配置项，用于 Redis队列
    - 调整 cache 配置项，增加 database 和 prefix 参数
    - 优化 default 配置项的格式和注释

--- config/redis.php
+++ config/redis.php
@@ -19,11 +19,16 @@ return [
         'port'     => (int)env('REDIS_PORT', 6379),
         'database' => (int)env('REDIS_DB', 0),
         'pool'     => [ // 连接池配置
-                        'max_connections'    => 10,     // 连接池最大连接数
-                        'min_connections'    => 1,      // 连接池最小连接数
-                        'wait_timeout'       => 3,         // 从连接池获取连接最大等待时间
-                        'idle_timeout'       => 50,        // 连接池中连接空闲超时时间，超过该时间会被关闭，直到连接数为min_connections
-                        'heartbeat_interval' => 50,  // 心跳检测间隔，不要大于60秒
+                        'max_connections'    => 10,
+                        // 连接池最大连接数
+                        'min_connections'    => 1,
+                        // 连接池最小连接数
+                        'wait_timeout'       => 3,
+                        // 从连接池获取连接最大等待时间
+                        'idle_timeout'       => 50,
+                        // 连接池中连接空闲超时时间，超过该时间会被关闭，直到连接数为min_connections
+                        'heartbeat_interval' => 50,
+                        // 心跳检测间隔，不要大于60秒
         ],
     ],
     'cache'   => [ // <==== 新增
@@ -33,4 +38,11 @@ return [
                    'database' => 2,
                    'prefix'   => 'webman_cache-',
     ],
+    'queue'   => [ // <==== 新增
+                   'host'     => env('REDIS_HOST', 'localhost'),
+                   'password' => env('REDIS_AUTH', null),
+                   'port'     => (int)env('REDIS_PORT', 6379),
+                   'database' => env('REDIS_QUEUE_DB', 1),
+                   'prefix'   => env('APP_NAME', ''),
+    ]
 ];
